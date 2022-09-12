eef3341a0bdff4c69916b3f5f450d63e1fb1c527
Utsav <utsavpoddar99@gmail.com>
2022-09-13 02:54:10 +0530

    addition of Tiangong position using CelesTrak

--- script.js
+++ script.js
@@ -14,5 +14,36 @@
 
         document.getElementById("1").innerHTML="Longitude: "+longitude+"   Latitude: "+latitude;
 
-        setTimeout( this.fetchCord, 1000);
+        setTimeout( this.fetchCord, 3000);
+        setTimeout( fetchtss(), 3000);
    };
+
+    fetchtss =  function (){
+        fetch("https://tle.ivanstanojevic.me/api/tle/48274")
+        .then((response) => response.json())
+        .then((data2) => this.displayCord1(data2.line1 , data2.line2))
+      
+      
+      };
+      displayCord1 = function (line1 , line2){
+     
+    // Initialize the satellite record with this TLE
+    const satrec = satellite.twoline2satrec(line1,line2 );
+    // Get the position of the satellite at the given date
+    const date = new Date();
+    const positionAndVelocity = satellite.propagate(satrec, date);
+    const gmst = satellite.gstime(date);
+    const position = satellite.eciToGeodetic(positionAndVelocity.position, gmst);
+
+console.log(radians_to_degrees(position.longitude),radians_to_degrees(position.latitude));
+
+//console.log(position.height);
+   
+// RADIANS TO DEGREES 
+function radians_to_degrees(radians)
+{
+  var pi = Math.PI;
+  return radians * (180/pi);
+}
+// RADIANS TO DEGREES 
+      };
