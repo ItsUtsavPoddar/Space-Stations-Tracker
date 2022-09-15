5b641245d05ab25450c2955f7b07ca02334adf0d
Utsav <utsavpoddar99@gmail.com>
2022-09-15 20:31:37 +0530

    Clean Code of working prototype

--- map.js
+++ map.js
@@ -1,21 +1,39 @@
-var map = L.map('map');
+// initializing the map on the "map" div with a given center and zoom
+
+var map = L.map('map', {
+    center: [0,0],
+    zoom: 1
+});
+
+//Instantiates a tile layer object given a URL template (Here its google Hybrid Map image) with its default attributes
 
 L.tileLayer('http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}',{
     maxZoom: 20,
     subdomains:['mt0','mt1','mt2','mt3']
 }).addTo(map);
 
+
+// from Leaflet.Terminator Library (https://github.com/joergdietrich/Leaflet.Terminator)
+
 var terminator = L.terminator().addTo(map);
 
-map.setView([0,0],1);
+// Interval function to call setTime() method without an argument to refresh the terminator to the current time. (EVERY SECOND SPECIFIED BY 1000 ms)
+setInterval(function() {
+    terminator.setTime();
+}, 1000);    
+    
 
 
-   var iconiss = L.icon({
+//Represents an icon to provide when creating a marker
+
+//icon for ISS
+var iconiss = L.icon({
     iconUrl:"https://iconarchive.com/download/i106861/goodstuff-no-nonsense/free-space/international-space-station.ico",
     iconSize: [50, 30],
     iconAnchor: [25, 15],
 })
 
+//icon for TSS
 var icontss = L.icon({
     iconUrl:"https://upload.wikimedia.org/wikipedia/commons/thumb/e/eb/Chinese_large_orbital_station.png/220px-Chinese_large_orbital_station.png",
     iconSize: [50, 30],
@@ -23,29 +41,41 @@ var icontss = L.icon({
 })
 
 
-var satmarkeriss = L.marker([0, 0],{icon: iconiss}).addTo(map)
 
-var satcircleiss = L.circle([0, 0], {
-    weight:1,
-    opacity:0.4,
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: 0.2,
-    radius:  2200e3
-}).addTo(map);
+//FOR ISS
+    //Instantiates a circle object given a geographical point, and an options object which contains the circle radius, etc
+    var satcircleiss = L.circle([0, 0], {
+        weight:1,
+        opacity:0.4,
+        color: 'red',
+        fillColor: '#f03',
+        fillOpacity: 0.2,
+        radius:  2200e3
+        }).addTo(map);
 
-var satmarkertss = L.marker([0, 0],{icon: icontss}).addTo(map)
+        // Instantiates a Marker object given a geographical point and optionally an options object. 
+        // Here it contains icon option
 
-var satcircletss = L.circle([0, 0], {
-    weight:1,
-    opacity:0.4,
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: 0.2,
-    radius:  2200e3
-}).addTo(map);
+        var satmarkeriss = L.marker([0, 0],{icon: iconiss}).addTo(map)
+
+//FOR TSS
+    //Instantiates a circle object given a geographical point, and an options object which contains the circle radius, etc
+    var satcircletss = L.circle([0, 0], {
+        weight:1,
+        opacity:0.4,
+        color: 'red',
+        fillColor: '#f03',
+        fillOpacity: 0.2,
+        radius:  2200e3
+        }).addTo(map);
+
+        // Instantiates a Marker object given a geographical point and optionally an options object. 
+        // Here it contains icon option
+
+        var satmarkertss = L.marker([0, 0],{icon: icontss}).addTo(map)
 
 
+// gets the cords from Script.js for marker and circle for ISS
 function locateiss(lat,long) {
 
     satmarkeriss.setLatLng([lat,long]);
@@ -53,6 +83,7 @@ function locateiss(lat,long) {
 
     };
 
+// gets the cords from Script.js for marker and circle for TSS 
 function locatetss(lat,long) {
 
         satmarkertss.setLatLng([lat,long]);
@@ -61,6 +92,7 @@ function locatetss(lat,long) {
         };
 
 
+// clicking on the map and you will see the coordinates in a popup (FROM DOCUMENTATION OF LEAFLET)      
 var popup = L.popup();
 
 function onMapClick(e) {
@@ -71,7 +103,3 @@ function onMapClick(e) {
 }
 map.on('click', onMapClick);
 
-setInterval(function() {
-	terminator.setTime();
-}, 1000);    
-    
--- script.js
+++ script.js
@@ -1,69 +1,83 @@
 
-   fetchiss = function (){
-    fetch("https://tle.ivanstanojevic.me/api/tle/25544")
-        .then((response) => response.json())
-        .then((data) => this.displayiss(data.line1 , data.line2));
+  fetchiss = function (){
+    fetch("https://tle.ivanstanojevic.me/api/tle/25544")           // fetching the data from celesTrak (TLE API) 
+        .then((response) => response.json())                       // pulling json file from the response 
+        .then((data) => this.displayiss(data.line1 , data.line2)); //Line 1 and Line 2 is from TLE format
    };
 
-   fetchtss =  function (){
-    fetch("https://tle.ivanstanojevic.me/api/tle/48274")
-    .then((response) => response.json())
-    .then((data2) => this.displaytss(data2.line1 , data2.line2));
+  fetchtss =  function (){
+    fetch("https://tle.ivanstanojevic.me/api/tle/48274")            // fetching the data from celesTrak (TLE API) 
+    .then((response) => response.json())                            // pulling json file from the response
+    .then((data) => this.displaytss(data.line1 , data.line2));      //Line 1 and Line 2 is from TLE format
   
   };
 
-  displayiss = function (line1 , line2){
-    // Initialize the satellite record with this TLE
-    const satrec = satellite.twoline2satrec(line1,line2 );
-    // Get the position of the satellite at the given date
-    const date = new Date();
-    const positionAndVelocity = satellite.propagate(satrec, date);
+  displayiss = function (line1 , line2){    
+    
+    const satrec = satellite.twoline2satrec(line1,line2 );    // Initializing the satellite record with the TLE (line 1 and line 2)
+    var date = new Date();
+
+    // Getting the position of the satellite at the given date 
+    // The position_velocity result is a key-value pair of ECI coordinates.
+    // https://celestrak.org/columns/v02n01/#:~:text=The%20ECI%20coordinate%20system%20(see,orthogonal%20(mutually%20perpendicular)%20axes.
+
+    var positionAndVelocity = satellite.propagate(satrec, date); 
+    
+    // grabbing GMST for the coordinate transforms.
+    // https://en.wikipedia.org/wiki/Sidereal_time#Definition
+
     const gmst = satellite.gstime(date);
-    const position = satellite.eciToGeodetic(positionAndVelocity.position, gmst);
 
-    const isslong = radians_to_degrees(position.longitude);
-    const isslat = radians_to_degrees(position.latitude);
+    // converts Earth-centered inertial ECI coordinates, specified by position, to latitude, longitude, altitude (LLA) geodetic coordinates.
+    const positionGd = satellite.eciToGeodetic(positionAndVelocity.position, gmst);
 
+    // Converting the RADIANS to DEGREES (given the results were in radians)
+    const isslong = satellite.degreesLong(positionGd.longitude);
+    const isslat = satellite.degreesLong(positionGd.latitude);
 
-console.log(isslong,isslat);
-locateiss(isslat,isslong);
-document.getElementById("ISS").innerHTML="Longitude: "+isslong+"   Latitude: "+isslat;
+    console.log(isslong.toFixed(4),isslat.toFixed(4));
 
-setTimeout( this.displayiss, 1000 ,line1 , line2);
+    locateiss(isslat,isslong); //map.js function
 
+    document.getElementById("ISS").innerHTML="Longitude: "+isslong.toFixed(4)+"   Latitude: "+isslat.toFixed(4); // printing on the HTML 
+
+    // doing recursion with same TLE data because TLE doesnt have to get updated every sec.
+
+    setTimeout( this.displayiss, 1000 ,line1 , line2);  
 };
 
-   
-    displaytss = function (line1 , line2){
-    // Initialize the satellite record with this TLE
-    const satrec = satellite.twoline2satrec(line1,line2 );
-    // Get the position of the satellite at the given date
-    const date = new Date();
-    const positionAndVelocity = satellite.propagate(satrec, date);
+
+  displaytss = function (line1 , line2){    
+    
+    const satrec = satellite.twoline2satrec(line1,line2 );    // Initializing the satellite record with the TLE (line 1 and line 2)
+    var date = new Date();
+
+    // Getting the position of the satellite at the given date 
+    // The position_velocity result is a key-value pair of ECI coordinates.
+    // https://celestrak.org/columns/v02n01/#:~:text=The%20ECI%20coordinate%20system%20(see,orthogonal%20(mutually%20perpendicular)%20axes.
+
+    var positionAndVelocity = satellite.propagate(satrec, date); 
+    
+    // grabbing GMST for the coordinate transforms.
+    // https://en.wikipedia.org/wiki/Sidereal_time#Definition
+
     const gmst = satellite.gstime(date);
-    const position = satellite.eciToGeodetic(positionAndVelocity.position, gmst);
 
-    const tsslong = radians_to_degrees(position.longitude);
-    const tsslat = radians_to_degrees(position.latitude);
+    // converts Earth-centered inertial ECI coordinates, specified by position, to latitude, longitude, altitude (LLA) geodetic coordinates.
+    const positionGd = satellite.eciToGeodetic(positionAndVelocity.position, gmst);
 
+    // Converting the RADIANS to DEGREES (given the results were in radians)
+    const tsslong = satellite.degreesLong(positionGd.longitude);
+    const tsslat = satellite.degreesLong(positionGd.latitude);
 
-console.log(tsslong,tsslat);
-locatetss(tsslat,tsslong);
-document.getElementById("TSS").innerHTML="Longitude: "+tsslong+"   Latitude: "+tsslat;
+    console.log(tsslong.toFixed(4),tsslat.toFixed(4));
 
-setTimeout( this.displaytss, 1000 ,line1 , line2);
+    locatetss(tsslat,tsslong); //map.js function
 
+    document.getElementById("TSS").innerHTML="Longitude: "+tsslong.toFixed(4)+"   Latitude: "+tsslat.toFixed(4); // printing on the HTML 
+
+    // doing recursion with same TLE data because TLE doesnt have to get updated every sec.
+    
+    setTimeout( this.displaytss, 1000 ,line1 , line2);  
 };
-
-//console.log(position.height);
-   
-// RADIANS TO DEGREES 
-function radians_to_degrees(radians)
-{
-  var pi = Math.PI;
-  const u = radians * (180/pi);
-  
-  return u.toFixed(4);
-}
-// RADIANS TO DEGREES 
 
