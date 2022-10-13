5100ab787f64ba091cdace3a23a8a58c8ff627b1
Utsav <utsavpoddar99@gmail.com>
2022-10-14 00:13:39 +0530

    bug removed (satellite overlap glitch)

--- script.js
+++ script.js
@@ -1,287 +1,227 @@
-
-{  //ULTIMATE CODE OF ISS
-
 var y;
-ultiss = function (check){
+var issOpen = true; // BOOLEAN VARIABLE TO TOGGLE ISS MARKERS
 
-  
-  if(check){
-    fetchiss();
-  }
-  else {
-    deanimate();
-  }
+yuck1 = function () {
+  var isscheckbox = document.getElementById("isscheckbox");
 
+  console.log(isscheckbox);
 
+  isscheckbox.addEventListener("click", function () {
+    if (issOpen) {
+      deanimate();
+      issOpen = false;
+    } else {
+      fetchiss();
+      issOpen = true;
+    }
+  });
+};
+deanimate = () => {
+    clearTimeout(y);
+    map.removeLayer(pathcordiss1);
+    map.removeLayer(pathcordiss2);
+    map.removeLayer(satmarkeriss);
+    map.removeLayer(satcircleiss);
 };
 
-
-
-var issOpen = false; // BOOLEAN VARIABLE TO TOGGLE ISS MARKERS
-
-
-yuck1 = function(){
-var isscheckbox = document.getElementById("isscheckbox");
-console.log(isscheckbox);
-isscheckbox.addEventListener("click" , function(){
-
-  if (issOpen){
-     ultiss(false);
-  issOpen= false;
-
-  }
-  else {
-    ultiss(true);
-  issOpen = true;
-  } 
-});}
-window.onload = yuck1();
-
-
-}
-
-
-
-  fetchiss = function (){
-    fetch("https://tle.ivanstanojevic.me/api/tle/25544")        // fetching the data from celesTrak (TLE API) 
-        .then((response) =>  response.json())                   // pulling json file from the response 
-        .then((data) => tledata(data.line1 , data.line2))       //Line 1 and Line 2 is from TLE format
+  fetchiss = function () {
+    fetch("https://tle.ivanstanojevic.me/api/tle/25544") // fetching the data from celesTrak (TLE API)
+      .then((response) => response.json()) // pulling json file from the response
+      .then((data) =>
+        tledata1(
+          "1 25544U 98067A   22286.50599244  .00016328  00000+0  29339-3 0  9996",
+          "2 25544  51.6412 110.4001 0003484 302.2718 151.8519 15.50007366363533"
+        )
+      ); //Line 1 and Line 2 is from TLE format
   };
 
+  tledata1 = function (line1, line2) {
+    console.log(line1, line2);
+    displayiss(line1, line2);
+    var pathcord = path(line1, line2);
+    pathcordiss1.setLatLngs(pathcord[0]);
+    pathcordiss2.setLatLngs(pathcord[1]);
+    //pathiss(pathcord);
+  };
 
+  displayiss = function (line1, line2) {
 
-  tledata = function (line1 , line2){
-
-    
     map.addLayer(pathcordiss1);
     map.addLayer(pathcordiss2);
     map.addLayer(satmarkeriss);
     map.addLayer(satcircleiss);
-    console.log (line1 , line2)
-    displayiss(line1,line2);
-    var pathcord = path (line1,line2);
-    pathiss(pathcord);
-  }
 
-
-  displayiss = function (line1 , line2){    
-      
     var cords = this.cords(line1, line2);
     isslong = cords[0];
     isslat = cords[1];
 
     //console.log(isslong.toFixed(4),isslat.toFixed(4));
 
-    locateiss(isslat,isslong); //map.js function
+    satmarkeriss.setLatLng([isslat, isslong]);
+    satcircleiss.setLatLng([isslat, isslong]);
+    //locateiss(isslat,isslong); //map.js function
 
-    document.getElementById("ISS").innerHTML="Longitude: "+isslong.toFixed(4)+"   Latitude: "+isslat.toFixed(4); // printing on the HTML 
+    document.getElementById("ISS").innerHTML =
+      "Longitude: " + isslong.toFixed(4) + "   Latitude: " + isslat.toFixed(4); // printing on the HTML
 
     // doing recursion with same TLE data because TLE doesnt have to get updated every sec.
     // this.l1 = line1; this.l2 = line2;
-    this.y = setTimeout( this.displayiss, 1000 ,line1 , line2); 
-}
+    this.y = setTimeout(this.displayiss, 1000, line1, line2);
+  };
 
-    deanimate = () => {
-      
-      clearTimeout(y);
-      map.removeLayer(pathcordiss1);
-      map.removeLayer(pathcordiss2);
-      map.removeLayer(satmarkeriss);
-      map.removeLayer(satcircleiss);
 
+var x;
+var tssOpen = true; // BOOLEAN VARIABLE TO TOGGLE ISS MARKERS
+
+yuck2 = function () {
+  var tsscheckbox = document.getElementById("tsscheckbox");
+
+  console.log(tsscheckbox);
+
+  tsscheckbox.addEventListener("click", function () {
+    if (tssOpen) {
+      deanimate2();
+      tssOpen = false;
+    } else {
+      fetchtss();
+      tssOpen = true;
     }
+  });
+};
+deanimate2 = () => {
+    clearTimeout(x);
+    map.removeLayer(pathcordtss1);
+    map.removeLayer(pathcordtss2);
+    map.removeLayer(satmarkertss);
+    map.removeLayer(satcircletss);
+};
 
+  fetchtss = function () {
+    fetch("https://tle.ivanstanojevic.me/api/tle/48274") // fetching the data from celesTrak (TLE API)
+      .then((response) => response.json()) // pulling json file from the response
+      .then((data) =>
+        tledata2("1 48274U 21035A   22286.14597225  .00036693  00000+0  41413-3 0  9993" ,"2 48274  41.4740 220.1889 0000899 113.8930  17.9022 15.61761501 83216")
+      
+      ); //Line 1 and Line 2 is from TLE format
+  };
 
+  tledata2 = function (line1, line2) {
+    console.log(line1, line2);
+    displaytss(line1, line2);
+    var pathcord = path(line1, line2);
+    pathcordtss1.setLatLngs(pathcord[0]);
+    pathcordtss2.setLatLngs(pathcord[1]);
+    //pathiss(pathcord);
+  };
 
+  displaytss = function (line1, line2) {
 
-
-
-
-
-
-
-
-    /*
-
-{   //ULTIMATR CODE OF TSS
-  ulttss = function (check){
-
-    var x;
     map.addLayer(pathcordtss1);
     map.addLayer(pathcordtss2);
     map.addLayer(satmarkertss);
     map.addLayer(satcircletss);
 
-  fetchtss = function (){
-      fetch("https://tle.ivanstanojevic.me/api/tle/48274")           // fetching the data from celesTrak (TLE API) 
-          .then((response) => response.json())                       // pulling json file from the response 
-          .then((data) => tledata(data.line1 , data.line2))          //Line 1 and Line 2 is from TLE format
+    var cords = this.cords(line1, line2);
+    tsslong = cords[0];
+    tsslat = cords[1];
+
+    //console.log(isslong.toFixed(4),isslat.toFixed(4));
+
+    satmarkertss.setLatLng([tsslat, tsslong]);
+    satcircletss.setLatLng([tsslat, tsslong]);
+    //locateiss(isslat,isslong); //map.js function
+
+    document.getElementById("TSS").innerHTML =
+      "Longitude: " + tsslong.toFixed(4) + "   Latitude: " + tsslat.toFixed(4); // printing on the HTML
+
+    // doing recursion with same TLE data because TLE doesnt have to get updated every sec.
+    // this.l1 = line1; this.l2 = line2;
+    this.x = setTimeout(this.displaytss, 1000, line1, line2);
   };
 
-  tledata = function (line1 , line2){
-
-          displaytss(line1,line2);
-          var pathcord = path (line1,line2);
-          pathtss(pathcord);
-
-  }
-
-
-  displaytss = function (line1 , line2){    
-      
-      var cords = this.cords(line1, line2);
-      tsslong = cords[0];
-      tsslat = cords[1];
-
-      console.log(tsslong.toFixed(4),tsslat.toFixed(4));
-
-      locatetss(tsslat,tsslong); //map.js function
-
-      document.getElementById("TSS").innerHTML="Longitude: "+tsslong.toFixed(4)+"   Latitude: "+tsslat.toFixed(4); // printing on the HTML 
-
-      // doing recursion with same TLE data because TLE doesnt have to get updated every sec.
-      // this.l1 = line1; this.l2 = line2;
-      this.x = setTimeout( this.displaytss, 1000 ,line1 , line2); 
-  }
-      
-
-  // gets the cords from Script.js for marker and circle for ISS
-  locatetss = function (lat,long) {
-
-      satmarkertss.setLatLng([lat,long]);
-      satcircletss.setLatLng([lat,long]);
-
-  };
-
-  pathtss = function (pathcord){
-
-      pathcordtss1.setLatLngs( pathcord[0]);
-      pathcordtss2.setLatLngs( pathcord[1]);
-
-  }
-
-  deanimate = () => {
-  
-    clearTimeout(this.x);
-    map.removeLayer(pathcordtss1);
-    map.removeLayer(pathcordtss2);
-    map.removeLayer(satmarkertss);
-    map.removeLayer(satcircletss);
-  }
-  if(check){
-    fetchtss();
-  }
-  else {
-    deanimate();
-  }
-
-  cords = function (line1 , line2){
-  
-  
-    const satrec = satellite.twoline2satrec(line1,line2 );    // Initializing the satellite record with the TLE (line 1 and line 2)
-    var date = new Date();
-  
-    //date = new Date (date.getTime() + 800000); // <-- TEST CODE (DO NOT UNCOMMENT THE CODE IF YOU DONT KNOW WHAT YOU ARE DOING)
-  
-    // Getting the position of the satellite at the given date 
-    // The position_velocity result is a key-value pair of ECI coordinates.
-    // https://celestrak.org/columns/v02n01/#:~:text=The%20ECI%20coordinate%20system%20(see,orthogonal%20(mutually%20perpendicular)%20axes.
-    
-    var positionAndVelocity = satellite.propagate(satrec, date); 
-    
-    // grabbing GMST for the coordinate transforms.
-    // https://en.wikipedia.org/wiki/Sidereal_time#Definition
-    
-    const gmst = satellite.gstime(date);
-    
-    // converts Earth-centered inertial ECI coordinates, specified by position, to latitude, longitude, altitude (LLA) geodetic coordinates.
-    const positionGd = satellite.eciToGeodetic(positionAndVelocity.position, gmst);
-    
-    // Converting the RADIANS to DEGREES (given the results were in radians)
-    const long = (180*positionGd.longitude)/Math.PI;
-    const lat = (180*positionGd.latitude)/Math.PI;
-    
-        return [long,lat];
-    
-  };
-
-  path = function(line1 , line2){
-
-    var pathC1 = [];
-    var pathC2 =[];
-    const satrec = satellite.twoline2satrec(line1,line2 );
-    var date = new Date();
-    var i = 0;
-
-    console.log(date);
-    for (; i<5500 ; i++) { 
-
-    var positionAndVelocity = satellite.propagate(satrec, date);
-    const gmst = satellite.gstime(date);
-    const positionGd = satellite.eciToGeodetic(positionAndVelocity.position, gmst);
-
-    const long = satellite.degreesLong(positionGd.longitude);
-    const lat = satellite.degreesLong(positionGd.latitude);
-      if (long < 179.8){
-
-        pathC1.push([lat,long]);
-      }
-      else if (long > 179.75){
-
-        break;
-      }
-    date = new Date(date.getTime() + 1000);
-  }
-
-  for (var j = 0; j<5500-i ; j++) { 
-
-    var positionAndVelocity = satellite.propagate(satrec, date);
-    const gmst = satellite.gstime(date);
-    const positionGd = satellite.eciToGeodetic(positionAndVelocity.position, gmst);
-
-    const long = satellite.degreesLong(positionGd.longitude);
-    const lat = satellite.degreesLong(positionGd.latitude);
-
-      if (long >= -180 && long <= 179 ){
-        pathC2.push([lat,long]);
-      }
-    
-    date = new Date(date.getTime() + 1000);
-  }
-  
-  console.log(pathC1,pathC2,i,j,date);
-  return([pathC1,pathC2]);
-}
-
-
-};
 
 
 
 
-var tssOpen = false; // BOOLEAN VARIABLE TO TOGGLE ISS MARKERS
 
 
 
-yuck2 = function(){
-var tsscheckbox = document.getElementById("tsscheckbox");
-console.log(tsscheckbox);
-tsscheckbox.addEventListener("click" , function(){
-
-  if (tssOpen){
-     ulttss(false);
-  tssOpen= false;
-
-  }
-  else {
-    ulttss(true);
-  tssOpen = true;
-  } 
-});}
-window.onload = yuck2();
-}
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 
  // OBSERVER FUNCTION
     me = function(){
@@ -326,5 +266,4 @@ window.onload = yuck2();
 
     console.log(azimuth , elevation, rangeSat);
 }
-
 */
